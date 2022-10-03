7e7221bd948e878120b9032ba1a5651357e9ce7d
Connor Roane <connor.roane@bottlerocketstudios.com>
2022-10-02 19:28:26 -0700

    write animation staggering with both adding class and ternary

--- src/components/global/Card.tsx
+++ src/components/global/Card.tsx
@@ -28,18 +28,29 @@ const CardContent = styled.div`
   width: 90%;
   height: 8rem;
 `
-const StyledCard = styled(Card)`
-width: 10rem;
-border: 0.125rem solid ${({ theme }) => theme.colors.navy};
-margin: 0.25rem;
-border-radius: 0.5rem;
-.card-title {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-&:hover ${CardContent} {
-  background: rgba(0, 0, 0, 0.4);
-}
+const StyledCard = styled(Card)<{ itemNumber: number; animate: boolean}>`
+  width: 10rem;
+  border: 0.125rem solid ${({ theme }) => theme.colors.navy};
+  margin: 0.25rem;
+  border-radius: 0.5rem;
+  .card-title {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  &:hover ${CardContent} {
+    background: rgba(0, 0, 0, 0.4);
+  }
+  transition: transform ${({ itemNumber }) => Math.sqrt(itemNumber)}s ease-in;
+  /* &.animate {
+    transform: translateX(6rem);
+  } */
+  ${({ itemNumber, animate }) => (
+    animate ? `
+      transform: translateX(6rem);
+    ` : `
+      
+    `
+  )}
 `
 export default StyledCard;
--- src/pages/Cards.tsx
+++ src/pages/Cards.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from '../components/global/Button';
 import Card from '../components/global/Card';
 import {Page, PageProps } from '../components/global/Page';
@@ -9,6 +9,13 @@ interface CardsProps extends PageProps {
 
 const Cards: React.FC<CardsProps> = ({}: CardsProps) => {
   const iterator = [1, 2, 3, 4, 5];
+  const [cardClass, setCardClass] = useState('');
+  const [animate, setAnimate] = useState(false);
+
+  const handleAnimateClick = () => {
+    setCardClass(cardClass ? '' : 'animate');
+    setAnimate(!animate);
+  }
 
   return (
     <>
@@ -16,10 +23,10 @@ const Cards: React.FC<CardsProps> = ({}: CardsProps) => {
         this is the cards page
       </div>
       <div>
-        {iterator.map((item, index) => <Card itemNumber={item} key={index} />)}
+        {iterator.map((item, index) => <Card className={cardClass} animate={animate} itemNumber={item} key={index} />)}
       </div>
       <div>
-        <Button>Animate Cards</Button>
+        <Button onClick={handleAnimateClick} >Animate Cards</Button>
       </div>
     </>
   )
